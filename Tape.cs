a24aaf8409a1cca17764404350ea0020e3baeeda
Kent Riggins <kent13@boohoo.us>
2024-06-29 21:25:01 -0400

    edited to trigger publish

--- Tape.cs
+++ Tape.cs
@@ -5,8 +5,6 @@
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
         public string Author { get; set; } = "";
-        public string URLofVideo { get; set; } = "";
-        
+        public string URLofVideo { get; set; } = "";        
     }
- 
 }
